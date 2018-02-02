3a6fcd2f5eeee59eaf0a70c595912b58eb616547
Oscar Ricaud <oiricaud@miners.utep.edu>
2018-02-02 16:26:08 -0700

    deleted the split class

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,19 @@ public class Main {
         System.out.println("Hello World!");
 
     }
-    public String split(String string, int size){
-
-        return "";
+    public String split(String s, int length){
+        String r = new String();
+        String sub;
+        for(int i = 0; i < s.length(); i+=length){
+            if(i+length > s.length()){
+                sub = s.substring(i, s.length());
+                r = r + sub;
+            }
+            else{
+                sub = s.substring(i, i+length);
+                r = r + sub + ", ";
+            }
+        }
+        return r;
     }
 }
