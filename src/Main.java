8b10448d8ccd84588a286433b5e060f0c0170ca8
mk <magiking@magiking.org>
2018-02-02 23:11:38 -0700

    Convert split to return String[]

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -5,7 +8,27 @@ public class Main {
     }
     
     public String [] split(String s, int length){
+        if(s.length() <= 0)
+            throw new IllegalArgumentException("String length must be greater than 0");
+        if(length <= 0)
+            throw new IllegalArgumentException("length must be a positive int");
+        if(length > Integer.MAX_VALUE)
+            throw new IllegalArgumentException("length too large");
+        if(s == null)
+            throw new IllegalArgumentException("String must be instantiated");
 
-        return new String[0];
+        List<String> r = new ArrayList<String>();
+        String sub;
+        for(int i = 0; i < s.length(); i+=length){
+            if(i+length > s.length()){
+                sub = s.substring(i, s.length());
+            }
+            else{
+                sub = s.substring(i, i+length); 
+            }
+            r.add(sub);
+        }
+        return r.toArray(new String[0]);
     }
+
 }
